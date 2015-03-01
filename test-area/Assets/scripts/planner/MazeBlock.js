d10b2bcced9c6eef8ba66ae36646cdda61e9c73c
Aaron Morris <aaorris@live.com>
2015-03-01 00:50:30 -0500

    updating scripts and unity project it's building some tiles programmatically, pretty cool

--- test-area/Assets/scripts/planner/MazeBlock.js
+++ test-area/Assets/scripts/planner/MazeBlock.js
@@ -1,7 +1,18 @@
-public class MazeBlock {
+public class MazeBlock extends MonoBehaviour {
+
+	public static var DEBUG : boolean = false;
+	public var model : GameObject;
+	public var instance : GameObject;
+	public static var scale : float = 5.0;
 
 	public var sides = {"UP":0,"RIGHT":0,"DOWN":0,"LEFT":0};
-	public var size : int;
+
+	public var x 						: int;
+	public var y 						: int;
+	public var marked 			: boolean;
+	public var full 				: boolean;
+	public var endPoint 		: boolean;
+	public var intersection : boolean;
 
 	/*
 	@inputs
@@ -10,8 +21,42 @@ public class MazeBlock {
 	@TODO: null
 	*/
 	public function MazeBlock() {
-
 	}
+	/*
+	@use
+		- called when initializing properties, and on forced resets.
+	@TODO: null
+	*/
+	function Reset () {
+		x = 0;
+		y = 0;
+		marked = false;
+		full = false;
+		endPoint = false;
+		intersection = false;
+	}
+	/*
+	@use called before first frame update
+	@TODO: null
+	*/
+	function Start () {
+		Debug.Log("Creating maze block");
+		instance = Instantiate(model, Vector3(x*scale,0,y*scale), Quaternion.identity);
+	}
+	/*
+	@inputs
+	@outputs
+	@use called before first frame update
+	@TODO: null
+	*/
+	function Update() {
+		if(DEBUG)
+			instance.transform.position = Vector3(x*scale,0,y*scale);
+	}
+	function OnDestroy() {
+		Debug.Log("Destroying");
+	}
+
 	/*
 	@inputs
 	@outputs
--- test-area/Assets/scripts/planner/MazeGenerator.js
+++ test-area/Assets/scripts/planner/MazeGenerator.js
@@ -1,15 +1,39 @@
 public class MazeGenerator extends MonoBehaviour {
 
-	var size : int;
-	var pieces : GameObject[];
+	var DEBUG : boolean = false;
+	var mapSize : int;
+	var scale : float = 3.0;
+	var models : GameObject[];
+	var pieces : Array;
 	/*
 	@inputs
 	@outputs
-	@use
-	@TODO: null
+	@use http://docs.unity3d.com/Manual/ExecutionOrder.html
 	*/
 	public function MazeGenerator() {
+	}
 
+	/*
+	@use
+		- called when initializing properties, and on forced resets.
+	@TODO: null
+	*/
+	function Reset () {
+		pieces = Array();
+		scale = 3.0;
+		MazeBlock.DEBUG = DEBUG;
+	}
+	/*
+	@use called before first frame update
+	@TODO: null
+	*/
+	function Start () {
+		MazeBlock.scale = scale;
+		GenerateMaze();
+	}
+	function Update() {
+		if(DEBUG)
+			MazeBlock.scale = scale;
 	}
 
 	/*
@@ -19,7 +43,18 @@ public class MazeGenerator extends MonoBehaviour {
 	@TODO: null
 	*/
 	public function GenerateMaze() {
-
+		pieces = Array();
+		Debug.Log("Generating maze of size " + mapSize);
+		for(var x=-mapSize; x< mapSize; x++) {
+			for(var y=-mapSize; y<mapSize; y++) {
+				Debug.Log(x+","+y);
+				var piece = gameObject.AddComponent(MazeBlock);
+				piece.model = models[0];
+				piece.x = x;
+				piece.y = y;
+				pieces.Add(piece);
+			}
+		}
 	}
 
 	/*
